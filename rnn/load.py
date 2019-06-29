a16ef292a5229f5f380ede7a6e1f65c874632b77
FlaviuVadan <flaviuvadan@gmail.com>
2019-06-29 15:51:11 -0600

    Switch to static methods

--- rnn/load.py
+++ rnn/load.py
@@ -1,27 +1,24 @@
 """ Load Shakespearian dataset """
 
-from __future__ import absolute_import, division, print_function
-
-import numpy as np
-import os
-import time
-
 import tensorflow as tf
+
 tf.enable_eager_execution()
 
 
 class Load:
     """ Load is responsible for downloading the Shakespeare text from TF """
 
-    source_url = 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt'
-
-    def get_path_to_file(self):
+    @staticmethod
+    def get_path_to_file():
         """
         Returns the path to the Shakespeare text
         :return: string - Shakespeare text
         """
-        return tf.keras.utils.get_file('shakespeare.txt', self.source_url)
+        source_url = 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt'
+        return tf.keras.utils.get_file('shakespeare.txt', source_url)
 
-    def get_text(self):
+    @staticmethod
+    def get_text():
         """ Returns """
-        return open(self.get_path_to_file(), 'rb').read().decode(encoding='utf-8')
+        path = Load.get_path_to_file()
+        return open(path, 'rb').read().decode(encoding='utf-8')
