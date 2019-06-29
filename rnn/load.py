50cbd19d574d52f382fead18c924d8c8cf9c26b8
FlaviuVadan <flaviuvadan@gmail.com>
2019-06-29 15:53:10 -0600

    Add vocabulary method

--- rnn/load.py
+++ rnn/load.py
@@ -12,13 +12,24 @@ class Load:
     def get_path_to_file():
         """
         Returns the path to the Shakespeare text
-        :return: string - Shakespeare text
+        :return: string - Shakespeare path
         """
         source_url = 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt'
         return tf.keras.utils.get_file('shakespeare.txt', source_url)
 
     @staticmethod
     def get_text():
-        """ Returns """
+        """
+        Returns the loaded Shakespeare text
+        :return: string - Shakespeare text
+        """
         path = Load.get_path_to_file()
         return open(path, 'rb').read().decode(encoding='utf-8')
+
+    @staticmethod
+    def get_vocabulary():
+        """
+        Returns the vocabulary/unique characters of the text
+        :return: set - vocabulary
+        """
+        return sorted(set(Load.get_text()))
